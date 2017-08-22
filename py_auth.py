1eb2f6391c436cace5a523956b60fcfbb24e091b
patricli <patricli@users.noreply.github.com>
2017-08-22 16:22:33 -0700

    Delete py_auth.py
    remove remote file

--- py_auth.py
+++
@@ -1,4 +0,0 @@
-consumer_key  	=  'FGqHveryX1eIvD56HKljFQdiM'
-consumer_secret =  'HeD79MvT3bKrX4h6nB9RClsFG9OaVgmRuj5AuQImRpJ1zcbcUZ'
-access_token 	=  '875698811099860992-yeDfyeQDRyOBTrO92Ecxfxi2alarFIf'
-access_token_secret  =  'iBPy6AgdIoC1CNGKkffgGjiVrihEuB3YGsbNiXKtt03VT'
