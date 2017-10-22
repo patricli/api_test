178620cadddc64d7c56a774cc35bc00df7f03157
patricli <patricli@users.noreply.github.com>
2017-10-22 18:41:59 -0400

    Update Chucktweets running in C9.io

--- Chucktweets.py
+++ Chucktweets.py
@@ -1,16 +1,12 @@
 import json
 import requests
 
+from flask import Flask, request
 from twython import Twython
-from py_auth import (
-    consumer_key,
-    consumer_secret,
-    access_token,
-    access_token_secret
-)
+from py_auth import *
 
-from spark_auth import (accessToken)
-from spark_env import (roomID)
+from Chuck_auth import (accessToken)
+
 
 #connect to TWTR
 twitter = Twython(
@@ -27,20 +23,32 @@ sparkHeaders = {
     'Authorization': 'Bearer '+accessToken
 }
 
+twUser = 'ChuckRobbins'  #Twitter screenname
 
-#Get timeline tweets
-twUser = 'ChuckRobbins'
-stat = twitter.get_user_timeline(count=12, screen_name=twUser)
-
-for d in stat:
-     #print(d['user']['screen_name'] + " tweeted " + d['text'])
-     message = d['user']['screen_name'] + " tweeted >>" + d['text']
-     sparkParam = {
-         'roomId' : roomID,
-         'text': message
-     }
-     r=requests.post(url = sparkURL, json = sparkParam, headers = sparkHeaders)
-     #print(r.status_code)
+#Init Flask web server
+app = Flask(__name__)
 
 
-r.close()
+@app.route('/', methods=['POST'])
+def hello():
+    filedata = request.get_json()
+    roomID = filedata['data']['roomId']
+    #roomID = 'Y2lzY29zcGFyazovL3VzL1JPT00vMWNkNjVkNjAtOGVjZC0xMWU3LWJmYTQtYzEzMzRiMGRlYjJj'
+
+    print('Initiate twitter')
+    stat = twitter.get_user_timeline(count=6, screen_name=twUser)  #Get tweet timelines
+    for d in stat:
+        message = d['user']['screen_name'] + " tweeted " + d['text']
+        #message = "Sendit"
+        sparkParam = {
+            'roomId' : roomID,
+            'text': message
+        }
+        print(sparkParam)
+        r=requests.post(url = sparkURL, data = json.dumps(sparkParam), headers = sparkHeaders)
+        print(r.status_code)
+    r.close()
+    return ""
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=False)
